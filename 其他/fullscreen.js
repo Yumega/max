d0394d94a9cebaf03bb873de90fbb7bf85d31619
Yumega <61570667+Yumega@users.noreply.github.com>
2021-11-19 06:59:14 +0800

    Update fullscreen.js

--- 其他/fullscreen.js
+++ 其他/fullscreen.js
@@ -1,3 +1,9 @@
+var delayInMilliseconds = 3000; //3 second
+
+setTimeout(function() {
+  //code to be executed after 3 second
+}, delayInMilliseconds);
+
 javascript:(function(){window.location=document.getElementsByTagName('video')[0].src})()
 
 completion();
