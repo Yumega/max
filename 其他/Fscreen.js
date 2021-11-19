c3beefe2e7cf272a49d0694cbeb0dae27a8bf7d5
Yumega <61570667+Yumega@users.noreply.github.com>
2021-11-19 16:38:44 +0800

    Create Fscreen.js

---
+++ 其他/Fscreen.js
@@ -0,0 +1,7 @@
+var delayInMilliseconds = 3000; //3 second
+
+setTimeout(function() {
+  //code to be executed after 3 second
+}, delayInMilliseconds);
+javascript:(function(){document.getElementsByTagName('video')[0].webkitSetPresentationMode("fullscreen");})()
+completion();
